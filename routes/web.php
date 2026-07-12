34418521bb498751503fe0dbfd0c0a90cbd1a04f
CalledJason <rendyhikmawan040@gmail.com>
2026-07-12 18:07:57 +0700

    fix: melanjutkan modul

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/posts', [PostController::class, 'index']);
+
+// Halaman Register
+Route::get('/register', [AuthController::class, 'showRegister'])
+    ->name('register');
+
+Route::post('/register', [AuthController::class, 'register']);
+
+
+// Halaman Login
+Route::get('/login', [AuthController::class, 'showLogin'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+
+// Logout
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
+
+
+// Halaman yang membutuhkan login
+Route::middleware('auth')->group(function () {
+
+    Route::get('/home', function () {
+        return view('home');
+    });
+
+});
