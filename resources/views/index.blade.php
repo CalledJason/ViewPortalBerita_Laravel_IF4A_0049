c98f374b65f9a49cd1fb977e0a47785c02c4a337
CalledJason <rendyhikmawan040@gmail.com>
2026-07-17 02:58:34 +0700

    feat: implement create post functionality

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,18 @@
+@if(session('success'))
+
+<div class="alert alert-success alert-dismissible fade show">
+
+    {{ session('success') }}
+
+    <button
+        class="btn-close"
+        data-bs-dismiss="alert">
+    </button>
+
+</div>
+
+@endif
+
 @extends('master')
 
 @section('title', 'Jason News')
