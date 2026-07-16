441d3c449b01a1744d9c8245bd8b190c3d3de213
CalledJason <rendyhikmawan040@gmail.com>
2026-07-17 00:31:37 +0700

    feat: implementasi resource post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,10 +8,89 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index() {
-        $featured = Post::first();
+        $featured = Post::latest()->first();
 
-        $posts = Post::where('id', '!=', $featured->id)->get();
+        $posts = Post::where('id', '!=', optional($featured)->id)
+                    ->latest()
+                    ->get();
 
-        return view('index', compact('featured', 'posts'));
+        $popularPosts = Post::latest()->take(5)->get();
+
+        return view('index', compact(
+            'featured',
+            'posts',
+            'popularPosts'
+        ));
+    }
+
+    public function create() {
+        return view('post_create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'=>'required|max:255',
+            'photo'=>'required',
+            'publisher'=>'required',
+            'content'=>'required',
+            'published'=>'required'
+        ]);
+
+        Post::create($request->all());
+
+        return redirect()
+            ->route('posts.index')
+            ->with('success','Berita berhasil ditambahkan');
+    }
+
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+
+        $relatedPosts = Post::where('id','!=',$id)
+                            ->latest()
+                            ->take(4)
+                            ->get();
+
+        return view('post_detail',compact(
+            'post',
+            'relatedPosts'
+        ));
+    }
+
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return view('post_edit',compact('post'));
+    }
+
+    public function update(Request $request,$id)
+    {
+        $request->validate([
+            'title'=>'required|max:255',
+            'photo'=>'required',
+            'publisher'=>'required',
+            'content'=>'required',
+            'published'=>'required'
+        ]);
+
+        $post = Post::findOrFail($id);
+
+        $post->update($request->all());
+
+        return redirect()
+            ->route('posts.index')
+            ->with('success','Berita berhasil diperbarui');
+    }
+
+    public function destroy($id)
+    {
+        Post::findOrFail($id)->delete();
+
+        return redirect()
+            ->route('posts.index')
+            ->with('success','Berita berhasil dihapus');
     }
 }
